a367929a86f7e91f33382a5486baa6c266b7a0d6
symbios <dxeqtr@gmail.com>
2018-06-28 18:00:07 +0300

    fix path

--- monitoring/src/main/java/ru/namibios/monitoring/utils/Const.java
+++ monitoring/src/main/java/ru/namibios/monitoring/utils/Const.java
@@ -2,8 +2,8 @@ package ru.namibios.monitoring.utils;
 
 public class Const {
 	
-	public static final String UPLOAD_DIR = "/home/symbios/fishingserver/resources/";
-	public static final String VERSION_DIR = "/home/symbios/fishingserver/version/";
+	public static final String UPLOAD_DIR = "/usr/local/fishing-resources/resources/";
+	public static final String VERSION_DIR = "/usr/local/fishing-resources/version/";
 	
 	public static final String UPDATED_ROW = "Updated %d rows";
 }
